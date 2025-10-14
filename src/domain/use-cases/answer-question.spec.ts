68d05e79520e94f33576cc85858bf30bb0a9df68
Juan Victor Leite <juanvictorleite@icloud.com>
2025-10-14 15:16:24 -0300

    feat: implement AnswerQuestionUseCase
    - Implemented AnswerQuestionUseCase using repository pattern
    - Added AnswersRepository interface

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,12 +1,20 @@
 import { describe, expect, it } from "vitest";
 
 import { Answer } from "../entities/answer";
+import type { AnswersRepository } from "../repositories/answers-repository";
 import { AnswerQuestionUseCase } from "./answer-question";
 
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (_answer: Answer) => {
+    return;
+  },
+};
+
 describe("Answer Question Use Case", () => {
-  it("should create an answer to a question", () => {
-    const useCase = new AnswerQuestionUseCase();
-    const answer = useCase.execute({
+  it("should create an answer to a question", async () => {
+    const useCase = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+    const answer = await useCase.execute({
       instructorId: "instructor-123",
       questionId: "question-456",
       content: "This is the answer to the question.",

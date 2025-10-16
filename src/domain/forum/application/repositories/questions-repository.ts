abfc2f0e7eb29591ff9429556d4f1452369c594d
Juan Victor Leite <juanvictorleite@icloud.com>
2025-10-16 15:39:42 -0300

    feat: implement GetQuestionBySlug use case and add findBySlug method to QuestionsRepository

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,5 +1,7 @@
 import type { Question } from "@/domain/forum/enterprise/entities/question";
 
 export interface QuestionsRepository {
+  findBySlug(slug: string): Promise<Question | null>;
+
   create(question: Question): Promise<void>;
 }

45cff05fe9aa502a6b43c909473702b858190e8f
Juan Victor Leite <juanvictorleite@icloud.com>
2025-10-16 15:55:34 -0300

    feat: add faker.js for generating random question data in makeQuestion factory

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,16 +1,23 @@
+import { faker } from "@faker-js/faker";
 import { UniqueEntityID } from "@/core/entities/value-objects/unique-entity-id";
 import {
   Question,
   type QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: "Example",
-    content: "Example content",
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 
   return question;
 }

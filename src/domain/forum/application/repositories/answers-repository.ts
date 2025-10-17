f213e02f8413536f161089709d15a9eac9a2a24a
Juan Victor Leite <juanvictorleite@icloud.com>
2025-10-17 11:19:40 -0300

    feat: implement DeleteAnswer use case with validation and in-memory repository support

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,5 +1,9 @@
 import type { Answer } from "../../enterprise/entities/answer";
 
 export interface AnswersRepository {
+  findById(id: string): Promise<Answer | null>;
+
   create(answer: Answer): Promise<void>;
+
+  delete(answer: Answer): Promise<void>;
 }

2e681b4a2a5492ec006c9afa9917a51a145d3feb
Juan Victor Leite <juanvictorleite@icloud.com>
2025-10-22 11:42:54 -0300

    test: fix edit-question spec

--- src/domain/forum/enterprise/entities/question-attachment.ts
+++ src/domain/forum/enterprise/entities/question-attachment.ts
@@ -1,7 +1,7 @@
 import { Entity } from "@/core/entities/entity";
 import type { UniqueEntityID } from "@/core/entities/value-objects/unique-entity-id";
 
-interface QuestionAttachmentProps {
+export interface QuestionAttachmentProps {
   questionId: UniqueEntityID;
   attachmentId: UniqueEntityID;
 }
